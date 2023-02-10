7f0bfa69c828f05872a4bf87cc8d2a8fa31ee375
Amir kangarloo <amirkangarloo1051@gmail.com>
2023-02-10 11:50:49 +0330

    feat: Implement ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
