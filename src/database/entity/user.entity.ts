8131d3b9accae06aeb4def3a675e0acdaa74a749
Amir kangarloo <amirkangarloo1051@gmail.com>
2023-02-13 14:19:43 +0330

    feat: Update user entity

--- src/database/entity/user.entity.ts
+++ src/database/entity/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
+import { UserStatusEnum } from "../../enum";
 import { BaseEntity } from "./base.entity";
 
 @Entity({ name: 'user' })
@@ -8,7 +9,33 @@ export class UserEntity extends BaseEntity {
 
     @Column({ unique: true })
     email: string;
+    @Column({ name: 'email_validation', type: "bool", default: false })
+    emailValidation: boolean;
 
     @Column()
     password: string;
+
+    @Column({ name: 'first_name', nullable: true })
+    firstName: string;
+    
+    @Column({ name: 'last_name', nullable: true })
+    lastName: string;
+    
+    @Column({ name: 'full_name', nullable: true,  })
+    fullName: string;
+
+    @Column({ name: 'profile_image', nullable: true })
+    profileImage: string;
+
+    @Column({ nullable: true })
+    bio: string;
+
+    @Column({ type: "enum", enum: UserStatusEnum, default: UserStatusEnum.INACTIVE })
+    status: UserStatusEnum;
+
+    @BeforeInsert()
+    insertFullName() {
+        this.fullName = `${this.firstName ?? ''} ${this.lastName ?? ''}`;
+    }
+
 }
---
+++ src/enum/user-status.enum.ts
@@ -0,0 +1,4 @@
+export enum UserStatusEnum {
+    INACTIVE = 'inactive',
+    ACTIVE = 'active'
+}
