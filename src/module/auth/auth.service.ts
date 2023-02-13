da9988633e5439ee5748fbf4ea83543bdbe1bf13
Amir kangarloo <amirkangarloo1051@gmail.com>
2023-02-13 16:36:08 +0330

    feat: Implement register

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { LoginUserDto, RegisterUserDto } from '../../dto';
+import * as bcrypt from 'bcrypt';
+import { UserEntity } from '../../database/entity';
 
 @Injectable()
 export class AuthService {
 
     public async register(data: RegisterUserDto) {
-        return `User with username: ${data.username} register.`;
+        const { password, email, username } = data;
+        try {
+            const hash = await this.hashPassword(password);
+            const user = await UserEntity.create({ email, username, password: hash }).save();
+            return user;
+        } catch (error) {
+            throw error;
+        }
     }
 
     public async login(data: LoginUserDto) {
         return `User with username: ${data.loginId} login.`;
+    }
+
+    private async hashPassword(password: string): Promise<string> {
+        const saltOrRounds = 10;
+        const hash = await bcrypt.hash(password, saltOrRounds);
+        return hash;
+    }
+
+    private async comparePassword(password: string, hash: string): Promise<boolean> {
+        const isMatch = await bcrypt.compare(password, hash);
+        return isMatch;
     }
 }
