93eefa0bcaecf90511ddc24c2eef23b8e9e0b62c
Amir kangarloo <amirkangarloo1051@gmail.com>
2023-02-15 12:28:13 +0330

    feat: Implement swagger for API Docs

--- src/dto/register-user.dto.ts
+++ src/dto/register-user.dto.ts
@@ -1,5 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class RegisterUserDto {
+    @ApiProperty({
+        name: 'email',
+        type: 'string',
+        required: true,
+        uniqueItems: true,
+        example: 'amir@gmail.com',
+    })
     email: string;
+    
+    @ApiProperty({
+        name: 'username',
+        type: 'string',
+        required: true,
+        uniqueItems: true,
+        example: 'amir',
+    })
     username: string;
+
+    @ApiProperty({
+        name: 'password',
+        type: 'string',
+        required: true,
+        example: 'Amir1234*',
+    })
     password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('NestJs Chat Application')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs-api', app, document);
   await app.listen(port);
   return port
 }
