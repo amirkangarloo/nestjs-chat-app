1b0bdead7f48bafcc69c6fad6df6c60199335a95
Amir kangarloo <amirkangarloo1051@gmail.com>
2023-02-15 11:50:41 +0330

    feat: Implement user module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './module/auth/auth.module';
+import { UserModule } from './module/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    AuthModule
+    AuthModule,
+    UserModule
   ],
   controllers: [],
   providers: [],
